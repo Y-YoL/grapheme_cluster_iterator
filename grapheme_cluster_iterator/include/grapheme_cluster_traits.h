aff707e8ddd75e52e1094151b31bf0655b3dea48
Y-YoL <11359025+Y-YoL@users.noreply.github.com>
2018-06-10 14:21:28 +0900

    add calc_codepoint_size
    コードポイントのサイズ計算を実装

--- grapheme_cluster_iterator/include/grapheme_cluster_traits.h
+++ grapheme_cluster_iterator/include/grapheme_cluster_traits.h
@@ -6,24 +6,42 @@
 namespace yol::grapheme_cluster_traits {
 
 	struct utf16_traits {
+		/// <summary>
+		/// 文字の型
+		/// </summary>
 		using char_type = char16_t;
+
+		/// <summary>
+		/// コードポイントの型
+		/// </summary>
+		using codepoint_type = std::uint32_t;
+
+		using string_view = std::basic_string_view<char_type>;
+
 		static constexpr char_type null_char = u'\0';
 
 		/// <summary>
 		/// コードポイントのサイズを計算する
 		/// </summary>
-		/// <param name="c">コードポイントのサイズを計算する文字</param>
+		/// <param name="text">コードポイントのサイズを計算する文字</param>
 		/// <returns>コードポイントのサイズ</returns>
-		static std::size_t calc_codepoint_size(std::basic_string_view<char_type> c);
+		static std::size_t calc_codepoint_size(string_view text);
+
+		/// <summary>
+		/// コードポイントを取得する
+		/// </summary>
+		/// <param name="text">コードポイントを取得する文字</param>
+		/// <returns>コードポイント</returns>
+		static codepoint_type get_codepoint(string_view text);
 
 		/// <summary>
 		/// クラスタのサイズを計算する
 		/// </summary>
-		/// <param name="c">計算を開始する位置</param>
+		/// <param name="text">計算を開始する位置</param>
 		/// <returns>クラスタのサイズ</returns>
-		static std::size_t calc_cluster_size(std::basic_string_view<char_type> c)
+		static std::size_t calc_cluster_size(string_view text)
 		{
-			return calc_codepoint_size(c);
+			return calc_codepoint_size(text);
 		}
 	};
 }
--- grapheme_cluster_iterator/source/grapheme_cluster_traits/utf16_traits.cpp
+++ grapheme_cluster_iterator/source/grapheme_cluster_traits/utf16_traits.cpp
@@ -1,11 +1,28 @@
 ﻿
 #include "stdafx.h"
 #include "../../include/grapheme_cluster_traits.h"
+#include <icu.h>
 
 namespace yol::grapheme_cluster_traits {
 
-	std::size_t utf16_traits::calc_codepoint_size(std::basic_string_view<char_type> c)
+	std::size_t utf16_traits::calc_codepoint_size(string_view text)
 	{
-		return 1;
+		if (text.length() == 0) {
+			return 0;
+		}
+
+		auto cp = get_codepoint(text);
+		return U16_LENGTH(cp);
+	}
+
+	auto utf16_traits::get_codepoint(string_view text) -> codepoint_type
+	{
+		if (text.length() == 0) {
+			return 0;
+		}
+
+		char32_t c = {};
+		U16_GET(text, 0, 0, text.length(), c);
+		return c;
 	}
 }
