1b66eff01e2e95c0930777fcc63858be1b0342f6
Y-YoL <11359025+Y-YoL@users.noreply.github.com>
2018-07-04 21:25:23 +0900

    add wchar_traits

---
+++ UnitTest/wchar_traits_test.cpp
@@ -0,0 +1,79 @@
+﻿#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "grapheme_cluster_string.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	using string = yol::wchar_grapheme_cluster_string;
+
+	TEST_CLASS(WcharTraitsTest)
+	{
+	public:
+		TEST_METHOD(TestAsciiDistance)
+		{
+			string text(L"abcd");
+
+			auto size = std::distance(text.begin(), text.end());
+			Assert::AreEqual(4, static_cast<int>(size));
+		}
+
+		TEST_METHOD(TestSurrogatePairDistance)
+		{
+			string text(L"𧸐𡸴𣷹𣏓");
+
+			auto size = std::distance(text.begin(), text.end());
+			Assert::AreEqual(4, static_cast<int>(size));
+		}
+
+		TEST_METHOD(TestMultiByteDistance)
+		{
+			string text(L"あいうえお");
+
+			auto size = std::distance(text.begin(), text.end());
+			Assert::AreEqual(5, static_cast<int>(size));
+		}
+
+		TEST_METHOD(TestKanaDistance)
+		{
+			string text(L"ｱｲｳｴｵ");
+
+			auto size = std::distance(text.begin(), text.end());
+			Assert::AreEqual(5, static_cast<int>(size));
+		}
+
+		TEST_METHOD(TestReturnCodeDistance)
+		{
+			string text(L"\n\n\r\r\n");
+
+			auto size = std::distance(text.begin(), text.end());
+			Assert::AreEqual(4, static_cast<int>(size));
+		}
+
+		TEST_METHOD(TestMultiByteIterator)
+		{
+			string text(L"あいうえお");
+
+			auto it = text.begin();
+			Assert::IsTrue((*it).view() == L"あ");
+		}
+
+		TEST_METHOD(TestExtendDistance)
+		{
+			string text(L"ｶﾞｷﾞｸﾞｹﾞｺﾞ");
+
+			auto size = std::distance(text.begin(), text.end());
+			Assert::AreEqual(5, static_cast<int>(size));
+		}
+
+		TEST_METHOD(TestCodePoint)
+		{
+			wchar_t text[] = L"A";
+
+			auto cp = string::cluster_traits::get_codepoint(text);
+
+			Assert::AreEqual<unsigned>(cp, 0x41);
+		}
+	};
+}
---
+++ grapheme_cluster_iterator/source/grapheme_cluster_traits/wchar_traits.cpp
@@ -0,0 +1,53 @@
+﻿
+#include "stdafx.h"
+#include "../../include/grapheme_cluster_traits.h"
+
+namespace {
+	template<std::size_t>
+	struct base_traits_helper;
+
+	template<>
+	struct base_traits_helper<2>
+	{
+		using type = yol::grapheme_cluster_traits::utf16_traits;
+	};
+
+	// wcharの処理をutf_traitsに移譲する
+	using yol::grapheme_cluster_traits::wchar_traits;
+	using base_traits = typename base_traits_helper<sizeof(wchar_traits::char_type)>::type;
+
+	base_traits::string_view view_cast(wchar_traits::string_view text)
+	{
+		auto p1 = text.data();
+		const base_traits::char_type* p2;
+
+		static_assert(sizeof(&p1) == sizeof(&p2));
+		static_assert(sizeof(*p1) == sizeof(*p2));
+		memcpy(&p2, &p1, sizeof(&p2));
+		return { p2, text.length() };
+	}
+}
+
+namespace yol::grapheme_cluster_traits {
+
+	std::size_t wchar_traits::calc_codepoint_size(string_view text)
+	{
+		return base_traits::calc_codepoint_size(view_cast(text));
+	}
+
+	auto wchar_traits::get_codepoint(string_view text) -> codepoint_type
+	{
+		return base_traits::get_codepoint(view_cast(text));
+	}
+
+	/// <summary>
+	/// クラスタのサイズを計算する
+	/// </summary>
+	/// <param name="text">計算を開始する位置</param>
+	/// <returns>クラスタのサイズ</returns>
+	/// <remarks>http://unicode.org/reports/tr29/#Grapheme_Cluster_Boundary_Rules</remarks>
+	std::size_t wchar_traits::calc_cluster_size(string_view text)
+	{
+		return base_traits::calc_cluster_size(view_cast(text));
+	}
+}
